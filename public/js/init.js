48dd42e2f94fbc83725b77ec929b304bebcaf35b
koldka <khal.fays@gmail.com>
2018-05-25 21:52:41 +0200

    Index

---
+++ public/js/init.js
@@ -0,0 +1,24 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+$(document).ready(function(){
+  $('select').formSelect();
+
+
+
+  
+});
+
+$("#imgImp").change(function() {
+  readURL(this);
+});
+$(document).ready(function(){
+  $('form input').change(function () {
+    $('form p').text(this.files.length + " file(s) selected");
+  });
+});
