2a1c27a4e7d40eec6ea3ebfe207796cdc6a20f48
MUKHTAR <saqab.mukhtar@cgi.com>
2018-05-26 02:07:12 +0200

    Fonction ajoute

--- public/js/upload_img.js
+++ public/js/upload_img.js
@@ -26,3 +26,34 @@ $(document).ready(function() {
     uploadFiles(formData);
 	}); 
 }
+
+function uploadFiles(formData) {
+    $.ajax({
+        url: 'vision/upload_image',
+        method: 'post',
+        data: formData,
+        processData: false,
+        contentType: false,
+        xhr: function () {
+            var xhr = new XMLHttpRequest();
+
+            // Add progress event listener to the upload.
+            xhr.upload.addEventListener('progress', function (event) {
+                var progressBar = $('.progress-bar');
+
+                if (event.lengthComputable) {
+                    var percent = (event.loaded / event.total) * 100;
+                    progressBar.width(percent + '%');
+
+                    if (percent === 100) {
+                        progressBar.removeClass('active');
+                    }
+                }
+            });
+
+            return xhr;
+        }
+    }).done(handleSuccess).fail(function (xhr, status) {
+        alert(status);
+    });
+}
